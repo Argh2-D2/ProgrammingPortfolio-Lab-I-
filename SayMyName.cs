e55398b7e321779fa8ec9e78de1e9ebb598e320b
Jordan J. Mugenyi <jordanjamesxvi@outlook.com>
2021-10-27 08:49:24 +0100

    Update SayMyName.cs

--- SayMyName.cs
+++ SayMyName.cs
@@ -6,7 +6,6 @@ namespace L1._5_SayMyName
     {
         static void Main(string[] args)
         {
-            // Your codes here
             Console.WriteLine("What is your name?");
             string myName = Console.ReadLine();
             Console.WriteLine("Hello " + myName + "!");
